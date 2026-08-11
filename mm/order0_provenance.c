68836b99e1433fa957ef15c4ec681fd4f38964f2
Nanzhe Zhao <zhaonanzhe@xiaomi.com>
2026-08-11 20:24:16 +0800

    sync: 2026-08-11 20:24:16 - 3 files from nzzhao-ThinkCentre-M760t

--- mm/order0_provenance.c
+++ mm/order0_provenance.c
@@ -91,7 +91,6 @@ static const char * const order0_source_names[ORDER0_SOURCE_NR] = {
 	[ORDER0_SOURCE_SLAB] = "slab",
 	[ORDER0_SOURCE_ZSMALLOC] = "zsmalloc",
 	[ORDER0_SOURCE_PAGE_FRAG] = "page_frag",
-	[ORDER0_SOURCE_SPLICE] = "splice",
 };
 
 static const char * const order0_pcp_flow_names[ORDER0_PCP_FLOW_NR] = {
